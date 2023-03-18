a2c1d0ac39dfe434d5772ffa7c473ddfbdc3df55
salman6098 <128226497+salman6098@users.noreply.github.com>
2023-03-18 04:55:05 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+function myfunction() {
+
+  var myfunction = confirm("Registraction Only For 18+");
+  if (myfunction)
+  {
+    window.location.href = "contact-us.html";
+
+  }
+  else
+  {
+
+    console.log("not working")
+  }
+  
+
+}
+
